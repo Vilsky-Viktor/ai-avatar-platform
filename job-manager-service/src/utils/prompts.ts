01e4fbb4c7668bc7c1a81864ea39576d347d20b0
viktor-vilskyi <viktor@terrificlive.com>
2026-04-17 18:06:56 +0800

    fix happy emotion for digital twin mode

--- job-manager-service/src/utils/prompts.ts
+++ job-manager-service/src/utils/prompts.ts
@@ -542,6 +542,7 @@ export const generateTrainingPhotoSetData = (parameters: AvatarParameters, avata
         faceRecognition: { enabled: true, mediaPaths: [idPhotoSet.generated.frontSmile!], threshold: { min: 0.94, max: 0.96 }},
         loras: [
           { path: "models/qwen-edit-2511/loras/Qwen-Image-Edit-2511-Lightning-8steps-V1.0-bf16", scale: 0.5 },
+          ...(AvatarTypes.twin ? [{ path: "models/qwen-edit-2511/loras/Qwen-Image-Edit-InSubject", scale: 1.0 }] : []),
           { path: "models/qwen-edit-2511/loras/Qwen-PixelSmile", scale: 1.0 },
           { path: "models/qwen-edit-2511/loras/qwen-edit-skin", scale: 1.0 },
         ],
